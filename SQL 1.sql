6eb2561135bb2fd1203bf7836a32cef53f2aca51
Shivani Sukhavasi <109085257+942002@users.noreply.github.com>
2023-11-01 21:34:17 +0530

    Add files via upload

---
+++ SQL 1.sql
@@ -0,0 +1,40 @@
+create database firsttable
+use firsttable
+create table firs ( Id numeric(20)primary key, Names Varchar (255), Age numeric(2),Education varchar(255),Occupation varchar(255),Employmentstatus varchar(255),Phone numeric(10),Addresss varchar(255),Gender varchar(1) );
+insert into firs values (1,'Manendra Rao',46,'Tenth','Milk Merchant','Self-employed',8985306836,'Sattenapalli','M');
+insert into firs values (2,'Devi',37,'Seventh','Homemaker','Self-employed',9652306836,'Sattenapalli','F');
+insert into firs values (3,'Yamini',22,'Bachelors(BE)','Engineer','employed',7981084596,'Benguluru','F');
+insert into firs values (4,'Shivani',20,'PGDM','Student','unemployed',7981759162,'Sattenapalli','F');
+insert into firs values (5,'Chenna Kesava',19,'Bcom','Student','unemployed',8688479148,'Sattenapalli','M');
+
+select * from firs
+drop table firs
+create table firs ( Id numeric(20)primary key, Names Varchar (255), Age numeric(2),Education varchar(255),Occupation varchar(255),Employmentstatus varchar(255),Phone numeric(10),Addresss varchar(255),Gender varchar(1) );
+insert into firs values (1,'Manendra Rao',46,'Tenth','Milk Merchant','Self-employed',8985306836,'Sattenapalli','M');
+insert into firs values (2,'Devi',37,'Seventh','Homemaker','Self-employed',9652306836,'Sattenapalli','F');
+insert into firs values (3,'Yamini',22,'Bachelors(BE)','Engineer','employed',7981084596,'Benguluru','F');
+insert into firs values (4,'Shivani',20,'PGDM','Student','unemployed',7981759162,'Sattenapalli','F');
+insert into firs values (5,'Chenna Kesava',19,'Bcom','Student','unemployed',8688479148,'Sattenapalli','M');
+insert into firs values (6,'TVS',12,'Not Educated','None','Self-employed',8684567148,'Sattenapalli','M');
+select * from firs
+
+update firs set Employmentstatus = 'Intern' where Id= 4
+delete from firs where Id= 6
+select * from firs
+alter table firs add salary numeric(6)
+sp_help firs
+alter table firs drop column salary
+alter table firs add salary numeric(6)
+alter table firs alter column salary numeric(8)
+sp_help firs
+drop table firs
+Create table maste (
+DeptId numeric(4)primary key,Deptname varchar(255),employment varchar(255) );
+Insert maste values ( 1,'Headdept', 'Selfemployed');
+Insert maste values ( 2,'Headdept', 'Selfemployed');
+Insert maste values ( 3,'middledept', 'employed');
+Insert maste values ( 4,'lowerleveldept', 'unemployed');
+Insert maste values ( 5,'lowerleveldept', 'unemployed');
+select *  from maste
+
+
